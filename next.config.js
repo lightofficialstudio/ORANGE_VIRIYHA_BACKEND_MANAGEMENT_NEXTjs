c192f07ca8e8437845a74793f717ffbb77c9607d
lightofficialstudio <81398651+lightofficialstudio@users.noreply.github.com>
2024-03-24 16:53:40 +0700

    Update dependencies: @fullcalendar/common, @babel/preset-react

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
 const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
   '@fullcalendar/daygrid',
   '@fullcalendar/interaction',
   '@fullcalendar/react',
